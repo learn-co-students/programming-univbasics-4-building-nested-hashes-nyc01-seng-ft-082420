c6516f3d6fd8b25c88b62bc0c669bc256548a890
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-08-07 21:46:45 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,17 +3,20 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-   {railroads:  {
+   hash = base_hash
+   hash[:railroads]  = {
       pieces: 4, 
       rent_in_dollars: {}, 
       names: {}
     }
-    }
+    hash
+end
 end
 
 def monopoly_with_third_tier
-  base_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
-  base_hash
+  hash = base_hash
+  hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}
+  hash
 end
 
 
